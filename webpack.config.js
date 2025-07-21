b598decb215ca7fe7d1863c80f6400fb541d0be1
Grzegorz Futa <grzegorzfuta@wp.pl>
2025-07-21 22:13:10 +0200

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
 	entry: './js/app.js',
 	 module: {
     rules: [
-    //  { test: /\.js$/, use: 'babel-loader' }
+      { test: /\.js$/, use: 'babel-loader' }
 ]
 },
       resolve: {
